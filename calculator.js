eb64edd87b9aa91916be954b5c024bf5450eaf99
Alpha <112643496+ESWARVETLA-19@users.noreply.github.com>
2023-03-31 17:49:24 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,21 @@
+var buttons=document.querySelectorAll("button");
+var display=document.querySelector("#display");
+document.addEventListener("click", btn);
+
+function btn(e){
+    if (e.target.id==="ac"){
+        display.value=" ";
+    }
+    else if(e.target.id==="DEL"){
+        display.value=display.value.slice(0,-1);
+    }
+    else if(e.target.id==="="){
+        display.value=eval(display.value);
+    }
+    else if(e.target.id==="calculator" || e.target.id==="display"){
+        display.value=display.value;
+    }
+    else{
+        display.value+=e.target.id;
+    }
+}
